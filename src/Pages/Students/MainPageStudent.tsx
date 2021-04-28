0cdba890fd73f862650a82c6a03c35751caf7901
Matúš Kalanin <mato.kalanin@gmail.com>
2021-04-28 16:04:00 +0200

    main page student in progress

--- src/Pages/Students/MainPageStudent.tsx
+++ src/Pages/Students/MainPageStudent.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Button, Container, Row } from 'reactstrap';
 import NavigationBar from '../../Components/Navbar/NavigationBar';
+import styles from './MainPage.module.scss';
 
 const  MainPageStudent  = () => {
         return(
             <div>
                 <NavigationBar/>
+                <Container>
+                    <Row style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
+                        <h2>List of available courses</h2>
+                    </Row>
+                    <Row style={{display: 'flex', justifyContent: 'center'}} >
+                        <div className={styles.box_subject}>
+                            <h6>Software Testing</h6>
+                            <Button style={{marginLeft: 250,marginRight: 20}}> Code</Button>
+                            <Button> Stats</Button>
+                        </div>
+                   </Row>
+                   <Row style={{display: 'flex', justifyContent: 'center'}} >
+
+                        <div className={styles.box_subject}>
+                            <h6>Software Testing</h6>
+                            <Button style={{marginLeft: 250,marginRight: 20}}> Code</Button>
+                            <Button> Stats</Button>
+                        </div>
+                    </Row>
+
+                    <Row style={{display: 'flex', justifyContent: 'center'}} >
+
+                        <div className={styles.box_subject}>
+                            <h6>Software Testing</h6>
+                            <Button style={{marginLeft: 250,marginRight: 20}}> Code</Button>
+                            <Button> Stats</Button>
+                        </div>                   
+                    </Row>
+
+                </Container>
                 
             </div>
         )
